75a054ab9d1b74a6a93a5170ac9ec41cd5c547dc
Steffen Grohsschmiedt <steffen@logicalclocks.com>
2019-10-02 11:10:06 +0200

    Fix code duplication

--- hops/featurestore_impl/rest/rest_rpc.py
+++ hops/featurestore_impl/rest/rest_rpc.py
@@ -3,11 +3,25 @@ REST calls to Hopsworks Feature Store Service
 """
 
 import json
-import sys
 import os
 
 from hops import constants, util
 from hops.exceptions import RestAPIError
+
+
+def _http_get(resource_url, headers=None):
+    method = constants.HTTP_CONFIG.HTTP_GET
+    connection = util._get_http_connection(https=True)
+    response = util.send_request(connection, method, resource_url, headers=headers)
+    resp_body = response.read().decode('utf-8')
+    response_object = json.loads(resp_body)
+
+    if response.code != 200:
+        error_code, error_msg, user_msg = util._parse_rest_error(response_object)
+        raise RestAPIError("Could not fetch feature stores (url: {}), server response: \n " \
+                           "HTTP code: {}, HTTP reason: {}, error code: {}, error msg: {}, user msg: {}".format(
+            resource_url, response.code, response.reason, error_code, error_msg, user_msg))
+    return response_object
 
 
 def _get_featurestores():
@@ -20,30 +34,11 @@ def _get_featurestores():
     Raises:
         :RestAPIError: if there was an error in the REST call to Hopsworks
     """
-    method = constants.HTTP_CONFIG.HTTP_GET
-    connection = util._get_http_connection(https=True)
-    resource_url = (constants.DELIMITERS.SLASH_DELIMITER +
-                    constants.REST_CONFIG.HOPSWORKS_REST_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER +
-                    constants.REST_CONFIG.HOPSWORKS_PROJECT_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER +
-                    util.project_id() + constants.DELIMITERS.SLASH_DELIMITER +
-                    constants.REST_CONFIG.HOPSWORKS_FEATURESTORES_RESOURCE)
-    response = util.send_request(connection, method, resource_url)
-    resp_body = response.read().decode('utf-8')
-    response_object = json.loads(resp_body)
-    # for python 3
-    if sys.version_info > (3, 0):
-        if response.code != 200:
-            error_code, error_msg, user_msg = util._parse_rest_error(response_object)
-            raise RestAPIError("Could not fetch feature stores (url: {}), server response: \n " \
-                                 "HTTP code: {}, HTTP reason: {}, error code: {}, error msg: {}, user msg: {}".format(
-                resource_url, response.code, response.reason, error_code, error_msg, user_msg))
-    else:  # for python 2
-        if response.status != 200:
-            error_code, error_msg, user_msg = util._parse_rest_error(response_object)
-            raise RestAPIError("Could not fetch feature stores (url: {}), server response: \n " \
-                                 "HTTP code: {}, HTTP reason: {}, error code: {}, error msg: {}, user msg: {}".format(
-                resource_url, response.code, response.reason, error_code, error_msg, user_msg))
-    return response_object
+    return _http_get(constants.DELIMITERS.SLASH_DELIMITER +
+                     constants.REST_CONFIG.HOPSWORKS_REST_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER +
+                     constants.REST_CONFIG.HOPSWORKS_PROJECT_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER +
+                     util.project_id() + constants.DELIMITERS.SLASH_DELIMITER +
+                     constants.REST_CONFIG.HOPSWORKS_FEATURESTORES_RESOURCE)
 
 
 def _get_featurestore_metadata(featurestore):
@@ -60,36 +55,14 @@ def _get_featurestore_metadata(featurestore):
     Raises:
         :RestAPIError: if there was an error in the REST call to Hopsworks
     """
-    method = constants.HTTP_CONFIG.HTTP_GET
-    connection = util._get_http_connection(https=True)
-    resource_url = (constants.DELIMITERS.SLASH_DELIMITER +
-                    constants.REST_CONFIG.HOPSWORKS_REST_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER +
-                    constants.REST_CONFIG.HOPSWORKS_PROJECT_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER +
-                    util.project_id() + constants.DELIMITERS.SLASH_DELIMITER +
-                    constants.REST_CONFIG.HOPSWORKS_FEATURESTORES_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER +
-                    featurestore + constants.DELIMITERS.SLASH_DELIMITER +
-                    constants.REST_CONFIG.HOPSWORKS_FEATURESTORE_METADATA_RESOURCE)
-    response = util.send_request(connection, method, resource_url)
-    resp_body = response.read().decode('utf-8')
-    response_object = json.loads(resp_body)
-    # for python 3
-    if sys.version_info > (3, 0):
-        if response.code != 200:
-            error_code, error_msg, user_msg = util._parse_rest_error(response_object)
-            raise RestAPIError("Could not fetch featurestore metadata for featurestore: {} (url: {}), "
-                                 "server response: \n "
-                                 "HTTP code: {}, HTTP reason: {}, error code: {}, "
-                                 "error msg: {}, user msg: {}".format(
-                resource_url, featurestore, response.code, response.reason, error_code, error_msg, user_msg))
-    else:  # for python 2
-        if response.status != 200:
-            error_code, error_msg, user_msg = util._parse_rest_error(response_object)
-            raise RestAPIError("Could not fetch featurestore metadata for featurestore: {} (url: {}), "
-                                 "server response: \n " \
-                                 "HTTP code: {}, HTTP reason: {}, error code: {}, "
-                                 "error msg: {}, user msg: {}".format(
-                resource_url, featurestore, response.status, response.reason, error_code, error_msg, user_msg))
-    return response_object
+    return _http_get(constants.DELIMITERS.SLASH_DELIMITER +
+                     constants.REST_CONFIG.HOPSWORKS_REST_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER +
+                     constants.REST_CONFIG.HOPSWORKS_PROJECT_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER +
+                     util.project_id() + constants.DELIMITERS.SLASH_DELIMITER +
+                     constants.REST_CONFIG.HOPSWORKS_FEATURESTORES_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER +
+                     featurestore + constants.DELIMITERS.SLASH_DELIMITER +
+                     constants.REST_CONFIG.HOPSWORKS_FEATURESTORE_METADATA_RESOURCE)
+
 
 def _get_project_info(project_name):
     """
@@ -104,34 +77,11 @@ def _get_project_info(project_name):
     Raises:
         :RestAPIError: if there was an error in the REST call to Hopsworks
     """
-    method = constants.HTTP_CONFIG.HTTP_GET
-    connection = util._get_http_connection(https=True)
-    resource_url = (constants.DELIMITERS.SLASH_DELIMITER +
-                    constants.REST_CONFIG.HOPSWORKS_REST_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER +
-                    constants.REST_CONFIG.HOPSWORKS_PROJECT_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER +
-                    constants.REST_CONFIG.HOPSWORKS_PROJECT_INFO_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER +
-                    project_name)
-    response = util.send_request(connection, method, resource_url)
-    resp_body = response.read().decode('utf-8')
-    response_object = json.loads(resp_body)
-    # for python 3
-    if sys.version_info > (3, 0):
-        if response.code != 200:
-            error_code, error_msg, user_msg = util._parse_rest_error(response_object)
-            raise RestAPIError("Could not fetch project metadata for project: {} (url: {}), "
-                                 "server response: \n "
-                                 "HTTP code: {}, HTTP reason: {}, error code: {}, "
-                                 "error msg: {}, user msg: {}".format(
-                project_name, resource_url, response.code, response.reason, error_code, error_msg, user_msg))
-    else:  # for python 2
-        if response.status != 200:
-            error_code, error_msg, user_msg = util._parse_rest_error(response_object)
-            raise RestAPIError("Could not fetch project metadata for project: {} (url: {}), "
-                                 "server response: \n " \
-                                 "HTTP code: {}, HTTP reason: {}, error code: {}, "
-                                 "error msg: {}, user msg: {}".format(
-                project_name, resource_url, response.status, response.reason, error_code, error_msg, user_msg))
-    return response_object
+    return _http_get(constants.DELIMITERS.SLASH_DELIMITER +
+                     constants.REST_CONFIG.HOPSWORKS_REST_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER +
+                     constants.REST_CONFIG.HOPSWORKS_PROJECT_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER +
+                     constants.REST_CONFIG.HOPSWORKS_PROJECT_INFO_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER +
+                     project_name)
 
 
 def _get_credentials(project_id):
@@ -147,34 +97,11 @@ def _get_credentials(project_id):
     Raises:
         :RestAPIError: if there was an error in the REST call to Hopsworks
     """
-    method = constants.HTTP_CONFIG.HTTP_GET
-    connection = util._get_http_connection(https=True)
-    resource_url = (constants.DELIMITERS.SLASH_DELIMITER +
+    return _http_get(constants.DELIMITERS.SLASH_DELIMITER +
                     constants.REST_CONFIG.HOPSWORKS_REST_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER +
                     constants.REST_CONFIG.HOPSWORKS_PROJECT_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER +
                     project_id + constants.DELIMITERS.SLASH_DELIMITER +
                     constants.REST_CONFIG.HOPSWORKS_PROJECT_CREDENTIALS_RESOURCE)
-    response = util.send_request(connection, method, resource_url)
-    resp_body = response.read().decode('utf-8')
-    response_object = json.loads(resp_body)
-    # for python 3
-    if sys.version_info > (3, 0):
-        if response.code != 200:
-            error_code, error_msg, user_msg = util._parse_rest_error(response_object)
-            raise RestAPIError("Could not fetch credentials for project: {} (url: {}), "
-                                 "server response: \n "
-                                 "HTTP code: {}, HTTP reason: {}, error code: {}, "
-                                 "error msg: {}, user msg: {}".format(
-                project_id, resource_url, response.code, response.reason, error_code, error_msg, user_msg))
-    else:  # for python 2
-        if response.status != 200:
-            error_code, error_msg, user_msg = util._parse_rest_error(response_object)
-            raise RestAPIError("Could not fetch credentials for project: {} (url: {}), "
-                                 "server response: \n " \
-                                 "HTTP code: {}, HTTP reason: {}, error code: {}, "
-                                 "error msg: {}, user msg: {}".format(
-                project_id, resource_url, response.status, response.reason, error_code, error_msg, user_msg))
-    return response_object
 
 
 def _get_featuregroup_rest(featuregroup_id, featurestore_id):
@@ -191,10 +118,7 @@ def _get_featuregroup_rest(featuregroup_id, featurestore_id):
     Raises:
         :RestAPIError: if there was an error in the REST call to Hopsworks
     """
-    headers = {constants.HTTP_CONFIG.HTTP_CONTENT_TYPE: constants.HTTP_CONFIG.HTTP_APPLICATION_JSON}
-    method = constants.HTTP_CONFIG.HTTP_GET
-    connection = util._get_http_connection(https=True)
-    resource_url = (constants.DELIMITERS.SLASH_DELIMITER +
+    return _http_get(constants.DELIMITERS.SLASH_DELIMITER +
                     constants.REST_CONFIG.HOPSWORKS_REST_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER +
                     constants.REST_CONFIG.HOPSWORKS_PROJECT_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER +
                     util.project_id() + constants.DELIMITERS.SLASH_DELIMITER +
@@ -203,22 +127,6 @@ def _get_featuregroup_rest(featuregroup_id, featurestore_id):
                     constants.DELIMITERS.SLASH_DELIMITER +
                     constants.REST_CONFIG.HOPSWORKS_FEATUREGROUPS_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER
                     + str(featuregroup_id))
-    response = util.send_request(connection, method, resource_url, headers=headers)
-    resp_body = response.read().decode('utf-8')
-    response_object = json.loads(resp_body)
-    try:  # for python 3
-        if (response.code != 200):
-            error_code, error_msg, user_msg = util._parse_rest_error(response_object)
-            raise RestAPIError("Could not get the metadata of featuregroup (url: {}), server response: \n " \
-                               "HTTP code: {}, HTTP reason: {}, error code: {}, error msg: {}, user msg: {}".format(
-                resource_url, response.code, response.reason, error_code, error_msg, user_msg))
-    except:  # for python 2
-        if (response.status != 200):
-            error_code, error_msg, user_msg = util._parse_rest_error(response_object)
-            raise RestAPIError("Could not get the metadata of featuregroup (url: {}), server response: \n " \
-                               "HTTP code: {}, HTTP reason: {}, error code: {}, error msg: {}, user msg: {}".format(
-                resource_url, response.status, response.reason, error_code, error_msg, user_msg))
-    return response_object
 
 
 def _get_training_dataset_rest(training_dataset_id, featurestore_id):
@@ -236,9 +144,7 @@ def _get_training_dataset_rest(training_dataset_id, featurestore_id):
         :RestAPIError: if there was an error in the REST call to Hopsworks
     """
     headers = {constants.HTTP_CONFIG.HTTP_CONTENT_TYPE: constants.HTTP_CONFIG.HTTP_APPLICATION_JSON}
-    method = constants.HTTP_CONFIG.HTTP_GET
-    connection = util._get_http_connection(https=True)
-    resource_url = (constants.DELIMITERS.SLASH_DELIMITER +
+    return _http_get(constants.DELIMITERS.SLASH_DELIMITER +
                     constants.REST_CONFIG.HOPSWORKS_REST_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER +
                     constants.REST_CONFIG.HOPSWORKS_PROJECT_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER +
                     util.project_id() + constants.DELIMITERS.SLASH_DELIMITER +
@@ -246,23 +152,7 @@ def _get_training_dataset_rest(training_dataset_id, featurestore_id):
                     str(featurestore_id) +
                     constants.DELIMITERS.SLASH_DELIMITER +
                     constants.REST_CONFIG.HOPSWORKS_TRAININGDATASETS_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER
-                    + str(training_dataset_id))
-    response = util.send_request(connection, method, resource_url, headers=headers)
-    resp_body = response.read().decode('utf-8')
-    response_object = json.loads(resp_body)
-    try:  # for python 3
-        if (response.code != 200):
-            error_code, error_msg, user_msg = util._parse_rest_error(response_object)
-            raise RestAPIError("Could not get the metadata of featuregroup (url: {}), server response: \n " \
-                               "HTTP code: {}, HTTP reason: {}, error code: {}, error msg: {}, user msg: {}".format(
-                resource_url, response.code, response.reason, error_code, error_msg, user_msg))
-    except:  # for python 2
-        if (response.status != 200):
-            error_code, error_msg, user_msg = util._parse_rest_error(response_object)
-            raise RestAPIError("Could not get the metadata of featuregroup (url: {}), server response: \n " \
-                               "HTTP code: {}, HTTP reason: {}, error code: {}, error msg: {}, user msg: {}".format(
-                resource_url, response.status, response.reason, error_code, error_msg, user_msg))
-    return response_object
+                    + str(training_dataset_id), headers)
 
 
 def _get_online_featurestore_jdbc_connector_rest(featurestore_id):
@@ -273,9 +163,7 @@ def _get_online_featurestore_jdbc_connector_rest(featurestore_id):
     Returns:
         the http response
     """
-    method = constants.HTTP_CONFIG.HTTP_GET
-    connection = util._get_http_connection(https=True)
-    resource_url = (constants.DELIMITERS.SLASH_DELIMITER +
+    return _http_get(constants.DELIMITERS.SLASH_DELIMITER +
                     constants.REST_CONFIG.HOPSWORKS_REST_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER +
                     constants.REST_CONFIG.HOPSWORKS_PROJECT_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER +
                     os.environ[constants.ENV_VARIABLES.HOPSWORKS_PROJECT_ID_ENV_VAR] + constants.DELIMITERS.SLASH_DELIMITER +
@@ -284,20 +172,3 @@ def _get_online_featurestore_jdbc_connector_rest(featurestore_id):
                     constants.REST_CONFIG.HOPSWORKS_FEATURESTORES_STORAGE_CONNECTORS_RESOURCE +
                     constants.DELIMITERS.SLASH_DELIMITER +
                     constants.REST_CONFIG.HOPSWORKS_ONLINE_FEATURESTORE_STORAGE_CONNECTOR_RESOURCE)
-    response = util.send_request(connection, method, resource_url)
-    resp_body = response.read()
-    response_object = json.loads(resp_body)
-    # for python 3
-    if sys.version_info > (3, 0):
-        if response.code != 200:
-            error_code, error_msg, user_msg = util._parse_rest_error(response_object)
-            raise RestAPIError("Could not fetch feature stores (url: {}), server response: \n " \
-                               "HTTP code: {}, HTTP reason: {}, error code: {}, error msg: {}, user msg: {}".format(
-                resource_url, response.code, response.reason, error_code, error_msg, user_msg))
-    else:  # for python 2
-        if response.status != 200:
-            error_code, error_msg, user_msg = util._parse_rest_error(response_object)
-            raise RestAPIError("Could not fetch feature stores (url: {}), server response: \n " \
-                               "HTTP code: {}, HTTP reason: {}, error code: {}, error msg: {}, user msg: {}".format(
-                resource_url, response.code, response.reason, error_code, error_msg, user_msg))
-    return response_object
