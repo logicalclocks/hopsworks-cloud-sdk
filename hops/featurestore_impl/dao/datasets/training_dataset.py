2afa77181b9c4c469720ffa18c0f15772e151186
Fabio Buso <fabio@logicalclocks.com>
2020-11-19 17:50:16 +0100

    [HOPSWORKS-1751] Query replay for training datasets  (#42)

--- hops/featurestore_impl/dao/datasets/training_dataset.py
+++ hops/featurestore_impl/dao/datasets/training_dataset.py
@@ -1,5 +1,6 @@
 from hops import constants
 from hops.featurestore_impl.dao.common.featurestore_entity import FeaturestoreEntity
+from hops.featurestore_impl.dao.features.training_dataset_feature import TrainingDatasetFeature
 
 
 class TrainingDataset(FeaturestoreEntity):
@@ -25,3 +26,13 @@ class TrainingDataset(FeaturestoreEntity):
         self.data_format = training_dataset_json[constants.REST_CONFIG.JSON_TRAINING_DATASET_FORMAT]
         self.training_dataset_type = training_dataset_json[constants.REST_CONFIG.JSON_TRAINING_DATASET_TYPE]
         self.location = training_dataset_json[constants.REST_CONFIG.JSON_TRAINING_DATASET_LOCATION]
+
+    def _parse_features(self, features_json):
+        """
+        Parses a list of features in JSON format into a list of Feature objects
+        Args:
+            :features_json: json representation of the list of features
+        Returns:
+            a list of Feature objects
+        """
+        return list(map(lambda feature_json: TrainingDatasetFeature(feature_json), features_json))
